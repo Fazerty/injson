28a228f9f27f145f0feb1d581b1827c4336a7928
Fabien Dubail <fabien.dubail@gmail.com>
2018-10-19 14:11:18 +0200

    Use vuetify

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from './views/Home.vue';
+import CreateProject from './views/CreateProject.vue';
 
 Vue.use(Router);
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: Home,
+      name: 'createProject',
+      component: CreateProject,
     },
     {
       path: '/about',
