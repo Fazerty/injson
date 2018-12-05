dbd0e00d38f2a0eadde8f38e8389a981d7e0e1f2
Fabien <fabien.dubail@gmail.com>
2018-12-05 10:13:37 +0100

    Prepare typeorm installation

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import store from '@/plugins/store/store';
 import 'roboto-fontface/css/roboto/roboto-fontface.css';
 import 'material-design-icons-iconfont/dist/material-design-icons.css';
 import i18n from './plugins/i18n';
+import 'reflect-metadata';
 
 Vue.config.productionTip = false;
 
--- src/models/project.ts
+++ src/models/project.ts
@@ -1,10 +1,19 @@
-export class Project {
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
-    public id?: string;
+@Entity()
+export class Project {
+    @PrimaryGeneratedColumn()
+    public id?: number;
+    @Column()
     public icon?: string;
+    @Column()
     public title?: string;
+    @Column()
     public folder?: string;
+    @Column()
     public translationFolder?: string;
+    @Column()
     public keySearchRegx?: string;
+    @Column()
     public fileExtension?: string;
 }
